772ffb70c00c892bb6a60655c6b4ccf585d3f5ec
Lars <larsf2005@gmail.com>
2024-12-28 14:45:37 +0000

    tut2

--- src/tutorials/2/index.tsx
+++ src/tutorials/2/index.tsx
@@ -1,30 +1,55 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
+import { animated, useSpring, useSpringValue } from "@react-spring/three";
+import { Mesh } from "three";
+
+import { FullScreen } from "@app/components";
 
 import "./styles.css";
 import info from "./info.md";
-import { FullScreen } from "@app/components";
 
-const Cube = (props) => {
-  const meshRef = useRef(null);
+const Cube = (props: MeshProps) => {
+  const meshRef = useRef<Mesh>(null);
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const { scale } = useSpring({
+    scale: active ? 1 : hovered ? 0.6 : 0.5,
+    config: {
+      friction: 12
+    }
+  });
+  const color = scale.to({
+    range: [0.5, 1],
+    output: ["orange", "hotpink"]
+  });
 
   useFrame(() => {
-    meshRef.current.rotation.x += 0.01;
+    if (meshRef.current) meshRef.current.rotation.x += 0.01;
   });
 
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color="orange" />
-    </mesh>
+    <animated.mesh
+      ref={meshRef}
+      onPointerOver={(e) => setHovered(true)}
+      onPointerOut={(e) => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={scale}
+      {...props}
+    >
+      <boxGeometry args={[1.5, 1.5, 1.5]} />
+      <animated.meshStandardMaterial color={color} />
+    </animated.mesh>
   );
 };
 
 const Playground = () => {
   return (
-    <FullScreen centerContent info={info}>
+    <FullScreen centerContent stretch info={info}>
       <Canvas>
-        <Cube />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} intensity={1000} />
+        <Cube position={[-0.5, 0, 0]} />
+        <Cube position={[0.5, 0, 0]} />
       </Canvas>
     </FullScreen>
   );
---
+++ src/tutorials/2/info.md
@@ -0,0 +1,1 @@
+[TUT](https://www.youtube.com/watch?v=jAbK-4-_NOM)
